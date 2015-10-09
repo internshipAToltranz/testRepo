778de371cf07bbb0666c34c80b1d4802b75973f9
iaubain <iaubain@yahoo.fr>
2015-10-09 14:50:41 +0200

    Revert "Aubain"
    This reverts commit 080626cea521d52933863fdbfca6ce063eeb429a.

--- transport_project/build/generated-sources/ap-source-output/entities/SubLocation_.java
+++ transport_project/build/generated-sources/ap-source-output/entities/SubLocation_.java
@@ -4,7 +4,7 @@ import javax.annotation.Generated;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-10-09T13:16:52")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2015-10-05T16:56:08")
 @StaticMetamodel(SubLocation.class)
 public class SubLocation_ { 
 
